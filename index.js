3b51282861b27f9d0300eaeaa13a5c3bd8e3801a
thewolftravel <bittencourtkah@gmail.com>
2023-11-03 12:45:54 -0500

    added comments and animation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("Hello, I'm Karina Bittencourt!");
+window.addEventListener("scroll", setScrollVar);
+window.addEventListener("resize", setScrollVar);
+
+function setScrollVar() {
+  const htmlElement = document.documentElement;
+  const percentOfScreenHeightScrolled =
+    htmlElement.scrollTop / htmlElement.clientHeight;
+  htmlElement.style.setProperty(
+    "--scroll",
+    Math.min(percentOfScreenHeightScrolled * 100, 100)
+  );
+}
+
+setScrollVar();
